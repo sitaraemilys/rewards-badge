9715c84d37ccd73b6c49815031d594d8d2357c38
sitypop <sityemilyshah@gmail.com>
2016-07-26 16:45:02 +0100

    JQuery

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,41 +1,60 @@
 $(document).ready(function() {
 
-$('.card orange').draggable({
-    cursor: 'move',
-    helper: "clone"
+$('.draggable-intrinsic').draggable({
 });
 
-$("#container1").droppable({
-  drop: function(event, ui) {
-    var itemid = $(event.originalEvent.toElement).attr("itemid");
-    $('.box-item').each(function() {
-      if ($(this).attr("itemid") === itemid) {
-        $(this).appendTo("#container1");
-      }
-    });
-  }
+$('.draggable-extrinsic').draggable({
 });
+
 
 $("#intrinsic").droppable({
-  drop: function(event, ui) {
-    var itemid = $(event.originalEvent.toElement).attr("itemid");
-    $('.box-item').each(function() {
-      if ($(this).hasClass("btn btn-default box-item intrinsic")) {
-        $(this).appendTo("#intrinsic");
+  drop: function( event, ui ) {
+    $( ui.draggable).children().children().css( "class", "card orange" )
+      .removeClass("orange");
+      console.log($(ui.draggable).attr('class'));
+      if (($(ui.draggable).attr('class')) === 'draggable-intrinsic ui-draggable ui-draggable-dragging') {
+        $(ui.draggable).children().children().css( "class", "card").addClass( "green" );
+      } else {
+        $(ui.draggable).children().children().css( "class", "card").addClass( "red" );
       }
-    });
+  },
+
+  out: function( event, ui ) {
+    $( ui.draggable).children().children().css( "class", "card")
+    .removeClass("red", "green")
+    .addClass("orange");
   }
 });
 
 $("#extrinsic").droppable({
-  drop: function(event, ui) {
-    var itemid = $(event.originalEvent.toElement).attr("itemid");
-    $('.box-item').each(function() {
-      if ($(this).attr("itemid") === itemid) {
-        $(this).appendTo("#extrinsic");
+  drop: function( event, ui ) {
+    $(ui.draggable).children().children().css( "class", "card orange" )
+      .removeClass("orange");
+      console.log($(ui.draggable).attr('class'));
+      if (($(ui.draggable).attr('class')) === 'draggable-extrinsic ui-draggable ui-draggable-dragging') {
+        $(ui.draggable).children().children().css( "class", "card").addClass( "green" );
+      } else {
+        $(ui.draggable).children().children().css( "class", "card").addClass( "red" );
       }
-    });
+  },
+
+  out: function( event, ui ) {
+    $( ui.draggable).children().children().css( "class", "card")
+    .removeClass("red")
+    .removeClass("green")
+    .addClass("orange");
   }
 });
+
+$('.maslow').click(function(){
+  $(this).children().children().css("class", "card orange")
+    .removeClass("orange");
+  if ($(this).attr("id") === "correct"){
+    $(this).children().children().css( "class", "card").addClass("green");
+  } else {
+    $(this).children().children().css( "class", "card").addClass("red");
+  }
+});
+
 
 });
